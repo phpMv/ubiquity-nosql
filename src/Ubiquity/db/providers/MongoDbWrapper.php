f34873b463df1deb0943ce124ed43d0e57b2c468
jcheron <myaddressmail@gmail.com>
2020-08-23 03:56:56 +0200

    Update MongoDbWrapper.php

--- src/Ubiquity/db/providers/MongoDbWrapper.php
+++ src/Ubiquity/db/providers/MongoDbWrapper.php
@@ -25,7 +25,7 @@ class MongoDbWrapper extends AbstractDbNosqlWrapper {
 	protected $dbName;
 
 	protected function getBulk($operation, $collectionName) {
-		return self::$bulks[$operation][$collectionName] ?? new \MongoDB\Driver\BulkWrite();
+		return self::$bulks[$operation][$collectionName] ??= new \MongoDB\Driver\BulkWrite();
 	}
 
 	public function toUpdate(string $collectionName, $filter = [], $newValues = [], $options = []) {
